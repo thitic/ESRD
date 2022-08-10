4da980149bcdd7e5a3393ee6c1a47779402f86b1
thitic <95263916+thitic@users.noreply.github.com>
2022-08-10 08:29:57 +0700

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ESRD_Sample01.MvcApplication" Language="C#" %>
---
+++ Views/ContactPersonController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ESRD_Sample01;
+
+namespace ESRD_Sample01.Views
+{
+    public class ContactPersonController : Controller
+    {
+        private ESRDEntities db = new ESRDEntities();
+
+        // GET: ContactPerson
+        public ActionResult Index()
+        {
+            return View(db.ContactPerson.ToList());
+        }
+
+        // GET: ContactPerson/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactPerson contactPerson = db.ContactPerson.Find(id);
+            if (contactPerson == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactPerson);
+        }
+
+        // GET: ContactPerson/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ContactPerson/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,PersonName,Moblie,Email,Position")] ContactPerson contactPerson)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ContactPerson.Add(contactPerson);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contactPerson);
+        }
+
+        // GET: ContactPerson/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactPerson contactPerson = db.ContactPerson.Find(id);
+            if (contactPerson == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactPerson);
+        }
+
+        // POST: ContactPerson/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,PersonName,Moblie,Email,Position")] ContactPerson contactPerson)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contactPerson).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contactPerson);
+        }
+
+        // GET: ContactPerson/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactPerson contactPerson = db.ContactPerson.Find(id);
+            if (contactPerson == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactPerson);
+        }
+
+        // POST: ContactPerson/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ContactPerson contactPerson = db.ContactPerson.Find(id);
+            db.ContactPerson.Remove(contactPerson);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
